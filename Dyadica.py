0d17173a83778dae7117d7785303232368eae0ef
Brenn <brennaser@gmail.com>
2025-11-20 15:01:12 -0500

    Create Main Dyadica file

---
+++ Dyadica.py
@@ -0,0 +1,25 @@
+"""
+Main executable for Dyadica.
+
+author: Brenn Sermania
+version: 11/20/2025
+"""
+def save_event_data():
+    pass
+
+
+def save_profile_data():
+    pass
+
+
+def load_event_data():
+    pass
+
+
+def load_profile_data():
+    pass
+
+
+if __name__ == "__main__":
+    pass
+
