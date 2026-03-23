b374ca18987fc910c0728f39b12adced42139fc4
Ubuntu <ubuntu@ip-172-31-27-225.ec2.internal>
2026-03-23 16:07:26 +0000

    add socket.run condition

--- Dyadica.py
+++ Dyadica.py
@@ -394,7 +394,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    socket.run(app, debug=True, host="0.0.0.0")
+    socket.run(app, debug=True, host="0.0.0.0", allow_unsafe_werkzeug=True)
 
     # make sure all data is being saved
     with open('dyadica_data.json', 'x') as file:
